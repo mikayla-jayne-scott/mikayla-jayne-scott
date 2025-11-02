c18c5937705aa5f8ea70380fc2231264d1b2d412
mikayla-j-scott <mikaylascott996@gmail.com>
2025-11-02 18:49:44 -0500

    rebuild

---
+++ script.js
@@ -0,0 +1,11 @@
+const toggle = document.getElementById("darkModeToggle");
+
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+  toggle.textContent = document.body.classList.contains("dark")
+    ? "☀️ Light Mode"
+    : "🌙 Dark Mode";
+});
+function goToPage(page) {
+  window.location.href = page;
+}
